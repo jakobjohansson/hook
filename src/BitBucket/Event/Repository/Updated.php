b42f99226b580bc36bdf5e703ac1cb9a4fab4b6e
Jakob Johansson <jakobjohansson2@icloud.com>
2017-11-09 23:10:11 +0100

    add transfer and updated events

---
+++ src/BitBucket/Event/Repository/Updated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Hook\BitBucket\Event\Repository;
+
+use Hook\BitBucket\Event\Event;
+
+class Updated extends Event
+{
+    /**
+     * The user who made the changes.
+     *
+     * @var object
+     */
+    public $actor;
+
+    /**
+     * The repository.
+     *
+     * @var object
+     */
+    public $repository;
+
+    /**
+     * The changes made to the repository.
+     *
+     * @var object
+     */
+    public $changes;
+
+    /**
+     * Proxy the object properties into a string.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->actor->username . " just updated some details in the <a href='" . $this->repository->project->links->html->href . "'>" . $this->repository->full_name . "</a> repository.";
+    }
+}
--- tests/BitBucketHookTest.php
+++ tests/BitBucketHookTest.php
@@ -35,4 +35,22 @@ class BitBucketHookTest extends TestCase
 
         $this->assertSame($this->response(), "emmap1 <a href='https://bitbucket.org/account/user/teamawesome/projects/proj'>just forked</a> a repository from emmap1.");
     }
+
+    public function testRepoUpdatedEvent()
+    {
+        $this->event('repo:updated');
+
+        $this->payload($this->bitBucket['repo:updated']);
+
+        $this->assertSame($this->response(), "emmap1 just updated some details in the <a href='https://bitbucket.org/account/user/teamawesome/projects/proj'>team_name/repo_name</a> repository.");
+    }
+
+    public function testRpoTransferEvent()
+    {
+        $this->event('repo:transfer');
+
+        $this->payload($this->bitBucket['repo:transfer']);
+
+        $this->assertSame($this->response(), "emmap1 just transferred the <a href='https://bitbucket.org/account/user/teamawesome/projects/proj'>repo_name</a> repository to emmap1.");
+    }
 }
