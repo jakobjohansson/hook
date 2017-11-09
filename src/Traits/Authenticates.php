451474db821eebd76f74ee6b4e5d3c36c3b59e9a
Jakob Johansson <jakobjohansson2@icloud.com>
2017-11-09 20:10:15 +0100

    remove temporary payload method from trait since it's only used by github

--- src/Traits/Authenticates.php
+++ src/Traits/Authenticates.php
@@ -21,21 +21,4 @@ trait Authenticates
     abstract protected function auth();
 
     abstract protected function validate();
-
-    /**
-     * Return the payload temporarily for authorization.
-     *
-     * @return array
-     */
-    protected function getTemporaryPayload()
-    {
-        switch ($this->contentType) {
-            case 'application/json':
-                return file_get_contents('php://input');
-            break;
-            case 'application/x-www-form-urlencoded':
-                return \Request::input('payload');
-            break;
-        }
-    }
 }
